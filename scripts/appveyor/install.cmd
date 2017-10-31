427c8cdb5af5fecbc9d640ba9f9d909c9b815f5f
Howard Butler <howard@hobu.co>
2017-10-30 20:42:02 -0500

    DESTDIR again

--- scripts/appveyor/install.cmd
+++ scripts/appveyor/install.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-cmake --build . --target install --config Release
-REM DESTDIR=C:\projects\pdal\install cmake --build . --target install --config Release
+REM cmake --build . --target install --config Release
+DESTDIR=C:\projects\pdal\install cmake --build . --target install --config Release
 REM nmake /f Makefile install  DESTDIR=C:\projects\pdal\install
 cd c:\projects\pdal\install\osgeo4w64
-dir
+
 tar jcvf ..\pdal-%APPVEYOR_REPO_COMMIT%.tar.bz2 .
 copy c:\pdal-%APPVEYOR_REPO_COMMIT%.tar.bz2 c:\projects\pdal
 echo "OSGeo4W64 build will be uploaded to https://s3.amazonaws.com/pdal/osgeo4w/pdal-%APPVEYOR_REPO_COMMIT%.tar.bz2"
